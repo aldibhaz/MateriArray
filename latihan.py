59d31e5114add1be76951dd875db42a22af94755
Aldi <aldi.bhaz100@gmail.com>
2022-04-12 07:08:40 +0700

    commit pertama

---
+++ latihan.py
@@ -0,0 +1,24 @@
+def CetakJudul (str1,str2):
+    print(str1)
+    print(str2)
+
+def penambahan(angka1,angka2):
+    hasilTambah = angka1 + angka2
+    print(hasilTambah)
+    if(hasilTambah<10):
+        print("Hasil kurang dari 10")
+
+
+judul = "Latihan Python"
+print(judul)
+
+judul1 = "Function is Python"
+print(judul1)
+
+a=3
+b=4
+c=a*b
+print("Hasil Perkalian = ",c)
+
+CetakJudul = (judul,judul1)
+penambahan(5,4)
